a2fc1fe6ac17f69fbdec189964cffc4ade7e5c8c
Tigran Drmeyan <drmeyant@gmail.com>
2021-10-08 17:14:11 +0400

    chapter 3. classes

---
+++ src/chapter3/BooleanExample.java
@@ -0,0 +1,10 @@
+package chapter3;
+
+public class BooleanExample {
+    public static void main(String[] args) {
+
+        int a = 0b010101;
+        System.out.println(a);
+
+    }
+}
---
+++ src/chapter3/CharDemo.java
@@ -0,0 +1,13 @@
+package chapter3;
+
+public class CharDemo {
+
+    public static void main(String[] args) {
+        char c = 88;
+
+        char c1 = '\n';
+        System.out.print("TIKO\nDRMEYAN");
+
+
+    }
+}
