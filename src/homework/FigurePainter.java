d10e6de286bd42d6a5a8b75c946938f8c4f83db3
Tigran Drmeyan <drmeyant@gmail.com>
2021-10-09 18:26:42 +0400

    homeworks part 1

--- src/homework/FigurePainter.java
+++ src/homework/FigurePainter.java
@@ -5,12 +5,39 @@ public class FigurePainter {
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < i; j++) {
                 System.out.print("* ");
+            }
+            {
+                System.out.println();
+            }
+        }
+        for (int i = 0; i < 5; i++) {
+            for (int j = 5; j > i; j--) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 4; j > i; j--) {
+                System.out.print("* ");
 
 
             }
-            { System.out.println();
-
+            {
+                System.out.println();
+            }
         }
-        }
+        System.out.println();
     }
 }
+
+
+
+
+
