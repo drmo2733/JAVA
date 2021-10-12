315118fcf974bce7bea64412fac127048b581433
Tigran Drmeyan <drmeyant@gmail.com>
2021-10-12 20:49:01 +0400

    homeworks part 2

--- src/homework/FigurePainter.java
+++ src/homework/FigurePainter.java
@@ -32,8 +32,27 @@ public class FigurePainter {
             {
                 System.out.println();
             }
+        }System.out.println();
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 4; j > i; j--) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print(" *  ");
+
+            }
+            System.out.println();
         }
-        System.out.println();
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 4; j > i; j--) {
+                System.out.print(" *  ");
+            }
+            System.out.println();
+        }
     }
 }
 
