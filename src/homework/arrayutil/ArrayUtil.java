a25891c0fae931deb7bafe9d00bbe6a18ac0c059
Tigran Drmeyan <drmeyant@gmail.com>
2021-10-16 17:18:02 +0400

    arrayutil

---
+++ src/homework/arrayutil/ArrayUtil.java
@@ -0,0 +1,40 @@
+package homework.arrayutil;
+
+
+public class ArrayUtil {
+    public static void main(String[] args) {
+//        1
+        int[] array = {7, 18, 22, 28, 33, 38, 55, 65, 73, 99};
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+//        2
+        int maximum;
+        maximum = array[0];
+
+        for (int i = 1; i < 10; i++) {
+
+            if (array[i] > maximum) {
+                maximum = array[i];
+            }
+        }
+        System.out.println("maximum = " + maximum);
+
+//        3
+        int minimum;
+        minimum = array[0];
+        for (int i = 1; i <10; i++) {
+            if (array[i] < minimum) {
+                minimum = array[i];
+            }
+
+        }
+        System.out.println("minimum = " + minimum);
+
+//        4
+
+
+
+
+    }
+}
