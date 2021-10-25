e13f26a1909c60bb72fd60d93fabe12d11adfba9
Tigran Drmeyan <drmeyant@gmail.com>
2021-10-25 21:33:20 +0400

    last homework part4-5

--- src/homework/array2/ArrayBolola.java
+++ src/homework/array2/ArrayBolola.java
@@ -26,6 +26,39 @@ public class ArrayBolola {
             System.out.println("false");
         }
 
+        char[] bbolola = {'b', 'a', 'b', 'o', 'l', 'a'};
+        boolean isBob = false;
+        for (int i = 0; i < bbolola.length - 2; i++) {
+            if (bbolola[i] == 'b' && bbolola[i + 2] == 'b') {
+                isBob = true;
+                break;
+            }
+
+        }
+        System.out.println(isBob);
+
+
+        char[] text = {' ', ' ', ' ', 'b', 'a', 'r', 'e', 'v', ' ', ' '};
+        int startIndex = 0;
+        int endIndex = text.length - 1;
+
+        while (startIndex < endIndex && text[startIndex] == ' ') {
+            startIndex++;
+        }
+        while (startIndex < endIndex && text[endIndex] == ' ') {
+            endIndex--;
+        }
+        char[] result = new char[(endIndex - startIndex) + 1];
+
+
+        int index = 0;
+        for (int i = startIndex; i <= endIndex; i++) {
+            result[index++] = text[i];
+        }
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i]);
+        }
+//
 
 
     }
